b9687f592b5178f668922a062e8e08638d2a6c44
gustavohassegawa <gustavohassegawa@gmail.com>
2025-11-09 22:11:28 -0300

    Páginas atualizadas

--- src/app/comandos/print/page.tsx
+++ src/app/comandos/print/page.tsx
@@ -2,11 +2,11 @@ import { CodePageTemplate, type Block } from "@/components/templates/CodePageTem
 
 export default function PrintPage() {
     const pageTitle = "print";
-    const pageSubtitle = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem dignissimos reprehenderit minus cupiditate. Qui est expedita commodi rerum necessitatibus beatae ipsum debitis placeat tempora. Nemo aspernatur ipsa laborum aliquid beatae.";
+    const pageSubtitle = "A maneira que o computador \"fala\"";
     const codeTextCard = 
     {
         title: "print()",
-        contentText: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias error blanditiis deleniti rem dignissimos in id quis labore eveniet. Ducimus voluptatum quaerat officia itaque aliquid mollitia architecto at! Velit, temporibus.",
+        contentText: "O comando print é utilizado para mostrar um texto na tela do computador, podemos pensar que ele é a forma em que um computador \“fala\”",
         contentCode: `
             print("Hello World!")
         `,
