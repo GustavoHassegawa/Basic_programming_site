4e61c9da24c4553d3d21967555d5eb33071491f4
gustavohassegawa <gustavohassegawa@gmail.com>
2025-11-09 22:44:09 -0300

    Reiniciar site (build ID novo)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  generateBuildId: async () => 'build-' + Date.now(),
 };
 
 export default nextConfig;
